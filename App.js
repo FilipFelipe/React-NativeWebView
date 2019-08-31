d867de45668735819ecfd325050138d2084922d7
Filip Felipe <filip_junio@hotmail.com>
2019-08-30 21:22:44 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,46 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Fragment} from 'react';
+import {WebView } from 'react-native-webview';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    
+    <WebView
+    source={{uri: 'http://200.211.73.16:8081/TerminalWeb/Login.aspx?ReturnUrl=%2fTerminalWeb%2f'}}
+    
+  />
+  )
+  webView.getSettings().setDisplayZoomControls(false);
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+
+});
+
+export default App;
